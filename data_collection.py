20c8f8302caa0de9dd5f940eb90f58fc01923388
Ryza Rynazal <ryzary@Ryzas-MacBook-Air.local>
2020-06-11 23:02:17 +0900

    uploading data and data collection script

---
+++ data_collection.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 23:09:01 2020
+
+@author: ryzary
+"""
+
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = '/Users/ryzary/Desktop/projects/glassdoor/chromedriver'
+
+df = gs.get_jobs('Business Analyst',500,False,path,2)
+
+
+df.to_csv('glassdoor_jobs.csv',index=False)
